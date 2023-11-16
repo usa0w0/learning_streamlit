a5d989f6fe7ba45936d36952611400487fd4007b
usa0w0 <kingkazuma1130@gmail.com>
2023-11-16 17:48:33 +0900

    簡単なドキュメントを作成

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Hi Friends!')
+import streamlit as st
+
+st.title("Streamlit勉強中") # タイトル
+st.header("見出し") # ヘッダー
+st.subheader('小見出し')
+st.write("hogehoge") # 表示
+
+st.markdown("# マークダウンでもかけるよ！（h1）") # マークダウンで表示
+st.markdown("## 正直MDが便利すぎるんじゃないか？（h1）")
+st.text("text") # テキスト表示
